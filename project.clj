2d5c7ec45aede831562d1092c75453c2bbe92e44
Ning Sun <sunng@protonmail.com>
2018-05-30 22:36:01 +0800

    (feat) added skeleton

--- project.clj
+++ project.clj
@@ -3,4 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [environ "1.1.0" :scope "provided"]
+                 [cheshire "5.8.0" :scope "provided"]
+                 [circleci/clj-yaml "0.5.6" :scope "provided"]
+                 [clj-http "3.9.0" :scope "provided"]])
---
+++ src/stavka/protocols.clj
@@ -0,0 +1,10 @@
+(ns stavka.protocols)
+
+(defprotocol Source
+  )
+
+(defprotocol Updater
+  )
+
+(defprotocol Resolver
+  )
