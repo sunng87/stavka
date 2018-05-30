7ed8f324d993eb9975194e56112b44a1891f93ad
Ning Sun <sunng@protonmail.com>
2018-05-30 21:36:03 +0800

    (chore) initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to stavka
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/stavka/core_test.clj
@@ -0,0 +1,7 @@
+(ns stavka.core-test
+  (:require [clojure.test :refer :all]
+            [stavka.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
