0245132bcda25753ccc87762e534a0ccc952c428
Ning Sun <sunng@protonmail.com>
2018-05-31 23:02:31 +0800

    (feat) added some key functions

--- project.clj
+++ project.clj
@@ -4,7 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
-                 [environ "1.1.0" :scope "provided"]
-                 [cheshire "5.8.0" :scope "provided"]
-                 [circleci/clj-yaml "0.5.6" :scope "provided"]
-                 [clj-http "3.9.0" :scope "provided"]])
+                 [environ "1.1.0"]
+                 [cheshire "5.8.0"]
+                 [circleci/clj-yaml "0.5.6"]
+                 [clj-http "3.9.0"]
+                 [hawk "0.2.11"]])
--- src/stavka/protocols.clj
+++ src/stavka/protocols.clj
@@ -1,10 +1,11 @@
 (ns stavka.protocols)
 
 (defprotocol Source
-  )
+  (reload [this]))
 
 (defprotocol Updater
-  )
+  (start! [this])
+  (stop! [this]))
 
 (defprotocol Resolver
-  )
+  (resolve [this key]))
