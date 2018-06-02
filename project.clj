7f6a7d85145f808c722cecb27e33dce7c630b461
Ning Sun <sunng@protonmail.com>
2018-06-02 15:20:11 +0800

    (chore) leiningen meta data

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject stavka "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject stavka "0.1.0-SNAPSHOT"
+  :description "Stavka manages configuration, for your clojure application"
+  :url "https://github.com/sunng87/stavka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
@@ -9,4 +9,8 @@
                  [circleci/clj-yaml "0.5.6"]
                  [clj-http "3.9.0"]
                  [hawk "0.2.11"]]
-  :dev-dependencies [tortue/spy "1.4.0"])
+  :plugins [[lein-codox "0.9.5"]]
+  :codox {:output-path "target/codox"
+          :source-uri "https://github.com/sunng87/stavka/blob/master/{filepath}#L{line}"
+          :metadata {:doc/format :markdown}}
+  :deploy-repositories {"releases" :clojars})
