69827aa62ca7fed07e3735b4f8c7bb2782d29be4
Ning Sun <sunng@protonmail.com>
2020-04-25 11:06:18 +0800

    (chore) update clojure

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/sunng87/stavka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
                  [cheshire "5.8.1"]
                  [clj-commons/clj-yaml "0.6.0"]
                  [clj-http "3.9.1"]
