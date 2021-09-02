0b0e23f24db1a535576eb5f9fb67c18abadca64d
Ning Sun <sunng@protonmail.com>
2021-09-02 23:02:42 +0800

    chore: Update dependencies

--- project.clj
+++ project.clj
@@ -3,20 +3,20 @@
   :url "https://github.com/sunng87/stavka"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.10.1"]
-                 [cheshire "5.10.0"]
-                 [clj-commons/clj-yaml "0.7.1"]
-                 [clj-http "3.9.1"]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [cheshire "5.10.1"]
+                 [clj-commons/clj-yaml "0.7.107"]
+                 [clj-http "3.12.3"]
                  [hawk "0.2.11"]]
-  :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.12.7" :scope "test"]]
+  :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.15.2" :scope "test"]]
                    :jvm-opts ["-Dstavka.test.attr=yes"]}
              :example {:dependencies [;; jdbc
-                                      [org.clojure/java.jdbc "0.7.11"]
-                                      [org.hsqldb/hsqldb "2.5.0"]
+                                      [org.clojure/java.jdbc "0.7.12"]
+                                      [org.hsqldb/hsqldb "2.6.0"]
                                       ;; kubernetes
                                       [io.fabric8/kubernetes-client "4.9.1"]
                                       ;; vault
-                                      [amperity/vault-clj "1.0.0"]]
+                                      [amperity/vault-clj "1.0.6"]]
                        :source-paths ["examples"]}}
   :plugins [[lein-codox "0.10.4"]]
   :codox {:output-path "target/codox"
