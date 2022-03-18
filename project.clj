986e5b417872777e6783d6a78b513066e3d7356b
Ning Sun <sunng@protonmail.com>
2022-03-18 22:55:28 +0800

    feat: deps up

--- project.clj
+++ project.clj
@@ -4,9 +4,9 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.3"]]
-  :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.17.2" :scope "test"]
+  :profiles {:dev {:dependencies [[info.sunng/ring-jetty9-adapter "0.17.4" :scope "test"]
                                   [cheshire "5.10.2"]
-                                  [clj-commons/clj-yaml "0.7.107"]
+                                  [clj-commons/clj-yaml "0.7.108"]
                                   [clj-http "3.12.3"]
                                   [hawk "0.2.11"]
                                   [org.clojure/tools.cli "1.0.206"]
@@ -20,7 +20,7 @@
                                       ;; kubernetes
                                       [io.fabric8/kubernetes-client "5.12.2"]
                                       ;; vault
-                                      [amperity/vault-clj "1.0.6"]]
+                                      [amperity/vault-clj "1.1.2"]]
                        :source-paths ["examples"]}
 
              :minimal {:test-selectors {:default [#(= (str %) "stavka.minimal-test") :minimal]}}}
